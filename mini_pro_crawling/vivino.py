2106f125a692e89dd7d2b05137990d36388c5de1
myeongjang <jacobmjjo@gmail.com>
2018-12-04 19:36:08 +0900

    make miniproject
    crawl from vivino and save at mongodb

--- mini_pro_crawling/vivino.py
+++ mini_pro_crawling/vivino.py
@@ -4,6 +4,7 @@ import re
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from pymongo import MongoClient
 
 from selenium.webdriver.support.ui import WebDriverWait
 
@@ -12,8 +13,16 @@ keyword = input("What kind of wine you want to buy??")
 
 driver = webdriver.Chrome("C:/driver/chromedriver.exe")
 driver.get(main_url)
+
+client = MongoClient('localhost', 27017)
+db = client.project
+collection = db.crawling
+
+
 def main():
+    
     search()
+    wine_crawler()
 
 def search():
     driver.get(main_url)
@@ -30,33 +39,51 @@ def search():
         print("You searched wrong thing")
     
     
-   
-def crawler():    
+dict1={}   
+def wine_crawler():    
 
-    
-    for i in range(1,5):
-        soup = BeautifulSoup(driver.page_source, "lxml" )
-        wine_items = soup.select(".wine-card__content")
-        driver.find_element_by_xpath('//*[@id="next"]').click()
-        driver.implicitly_wait(10)
-        wine_items = soup.select(".wine-card__content")
-        for wine in wine_items:
-            title = wine.select(".link-color-alt-grey > .bold")[0].text.replace("\n","")
-            region = wine.select(".link-color-alt-grey")[1].text
-            country = wine.select(".link-color-alt-grey")[2].text
-            rating = wine.select(".text-inline-block")[0].text.strip().replace(" ","")
-            price = wine.select(".wine-price-value")[0].text.replace("—","0")
+    try:
+        
+
+        for i in range(1,5):
+            soup = BeautifulSoup(driver.page_source, "lxml" )
             
-            print(title, " : name")
-            print(region, " : region")
-            print(country, " : country")
-            print(rating, ": rating")
-            print(price, ": price")
+            driver.find_element_by_xpath('//*[@id="next"]').click()
+            driver.implicitly_wait(10)
+            wine_items = soup.select(".default-wine-card")
+            for wine in wine_items:
+                img_ex = wine.find("figure")['style']
+                img = re.findall("[/][/].+g",img_ex)
+                url = wine.find('a')['href']
+                title = wine.select(".link-color-alt-grey > .bold")[0].text.replace("\n","")
+                region = wine.select(".link-color-alt-grey")[1].text
+                country = wine.select(".link-color-alt-grey")[2].text
+                rating = wine.select(".text-inline-block")[0].text.strip().replace(" ","")
+                price = wine.select(".wine-price-value")[0].text.replace("—","0")
+                url = main_url+url
+                wineInfo = {
+                    'ImageURL' : "https:"+img[0],
+                    'URL' : url,
+                    'Name' : title,
+                    'Origin' : country,
+                    'Rate' : rating,
+                    'Price' : price 
+                }
+
+                collection.insert_one(wineInfo)
+
+                print("https:"+img[0], " : img")
+                print(main_url+url, " : url" )
+                print(title, " : name")
+                print(region, " : region")
+                print(country, " : country")
+                print(rating, ": rating")
+                print(price, ": price")
+    except Exception as e:
+       print("페이지 파싱 에러", e)
+    finally:
+        time.sleep(3)
+        driver.close()   
             
-      
-    
-       
-    
 if __name__ == "__main__":
-    search()
-    crawler()
+    main()
